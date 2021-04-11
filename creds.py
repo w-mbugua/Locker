be7d60bdb28a03c38c71a3bf610506f0a81b8033
JoyMbugua <wacumbugua@gmail.com>
2021-04-11 11:10:01 +0300

    added a save passwords method

--- creds.py
+++ creds.py
@@ -7,3 +7,7 @@ class Credentials:
         self.platform = platform
         self.username = username
         self.password = password
+    def save_cred(self):
+        Credentials.locker[self.platform] = {self.username: self.password} # nested dictionary
+        print(Credentials.locker)
+        print(len(Credentials.locker))
