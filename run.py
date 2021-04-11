98b94a4cae278d09020f524eaf8bff2b583a2c1a
JoyMbugua <wacumbugua@gmail.com>
2021-04-11 11:51:42 +0300

    created the main file layout

---
+++ run.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+while True:
+    choice = input("Enter 'create' to create new credentials, 'display' to view your saved credentials, 'delete' to delete credentials, or 'copy' to copy an account's password:\n")
+    if choice == "create":
+        pass
+    elif choice == "display":
+        pass
+    elif choice == "display":
+        pass
+    elif choice == "copy":
+        pass
+    else:
+        print("Incorrect input. Please try again")
+    
